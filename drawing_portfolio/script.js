b03d2d12112c20833eac9efa11b85152cd1ab3ab
Bishal-35 <karmakarbishal56@gmail.com>
2025-03-23 22:49:47 +0530

    initial commit for drawing portfolio

---
+++ drawing_portfolio/script.js
@@ -0,0 +1,26 @@
+// const slider = document.querySelector('.slider');
+
+// function activate(e) {
+//   const items = document.querySelectorAll('.item');
+//   e.target.matches('.next') && slider.append(items[0]);
+//   e.target.matches('.prev') && slider.append(items[items.ngth -1]);
+// }
+// document.addEventListener('click',activate,false);
+
+
+
+const slider = document.querySelector('.slider'); // Make sure your <main> has class="slider"
+
+function activate(e) {
+  const items = document.querySelectorAll('.item');
+  
+  if (e.target.matches('.next')) {
+    slider.append(items[0]);
+  }
+
+  if (e.target.matches('.prev')) {
+    slider.prepend(items[items.length - 1]);
+  }
+}
+
+document.addEventListener('click', activate, false);
